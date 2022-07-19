4f73803050df2189094528776ae55db58b47aa74
Nexuslolz <a.klyosov@inbox.ru>
2022-07-20 02:50:02 +0500

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,48 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let sentence = []
+    for(i = 0; i < expr.length; i++){
+        sentence.push(expr.slice(i, i + 2))
+        i += 1;
+    }
+    
+    let morseResult = []
+    for(i = 0; i < sentence.length; i++){
+        if(sentence[i] === '10'){
+            morseResult.push('.')
+        } else if(sentence[i] === '11'){
+            morseResult.push('-')
+        } else if(sentence[i] === '**'){
+            morseResult.push('**')
+        } else if(sentence[i] === '00'){
+            morseResult.push('')
+        }
+    }
+    let words = []
+    for(i = 0; i < morseResult.length; i++){
+        if(morseResult[i] === '**'){
+            words.push(' ')
+            i += 4;
+        } else {
+        words.push(morseResult.slice(i, i + 5).join(''))
+        i += 4;
+        }
+    }
+    let result = []
+    for(i = 0; i < words.length; i++){
+        for(let key in MORSE_TABLE){
+            if(words[i] === key){
+                result.push(Object.values(MORSE_TABLE[key]))
+            } else if(words[i] === ' '){
+                result.push(' ')
+            }
+  }
+  }
+  let output = result.join('')
+  
+  
+  return output.replace(/\s+/g,' ');
 }
 
 module.exports = {
